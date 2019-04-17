c4ad5b85c2d99d1be66b114f59b493b265a372c4
zloykoteyka145 <34992586+zloykoteyka145@users.noreply.github.com>
2019-04-17 22:55:16 +0300

    Add files via upload

---
+++ html/slide.php
@@ -0,0 +1,13 @@
+<div>
+    <div class="sliderimg">
+         <div class="img<?=$i?>">
+
+         </div>
+    </div>
+    <div class="slidertext">
+        <div class="slidername slidername<?=$i?>">
+        </div>
+        <div class="sliderdesc sliderdesc<?=$i?>">
+        </div>
+    </div>
+</div>
---
+++ html/slider.php
@@ -0,0 +1,44 @@
+<div class="slider">
+  <!-- 1 -->
+  <div>
+      <div class="sliderimg">
+           <div class="img1">
+
+           </div>
+      </div>
+      <div class="slidertext">
+          <div class="slidername slidername1">
+          </div>
+          <div class="sliderdesc sliderdesc1">
+          </div>
+      </div>
+  </div>
+  <!-- 2 -->
+  <div>
+      <div class="sliderimg">
+           <div class="img2">
+
+           </div>
+      </div>
+      <div class="slidertext">
+          <div class="slidername slidername2">
+          </div>
+          <div class="sliderdesc sliderdesc2">
+          </div>
+      </div>
+  </div>
+  <!-- 3 -->
+  <div>
+      <div class="sliderimg">
+           <div class="img3">
+
+           </div>
+      </div>
+      <div class="slidertext">
+          <div class="slidername slidername3">
+          </div>
+          <div class="sliderdesc sliderdesc3">
+          </div>
+      </div>
+  </div>
+</div>
