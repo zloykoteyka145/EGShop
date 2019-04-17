6e5893598a6a8da79da05152faccbf964e2082f0
zloykoteyka145 <34992586+zloykoteyka145@users.noreply.github.com>
2019-04-17 23:00:47 +0300

    Add files via upload

---
+++ html/card.php
@@ -0,0 +1,17 @@
+<div class="card">
+  <div class="cardimg">
+    <div class="img1">
+
+    </div>
+  </div>
+  <div class="cardtext">
+      <span class="cardname slidername1"></span>
+      <span class="carddesc sliderdesc1"></span>
+      <div class="cardprice">
+        <span class="sliderprice1"></span> ₽
+      </div>
+  </div>
+  <div class="cardbutton">
+    <span class="cardbuttontext">Купить</span>
+  </div>
+</div>
